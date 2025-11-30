ec7e0ca3c28faad80dda01becf61c4d5e0d9b18e
Mohammad Hadi <mind.hadi5@gmai.com>
2025-11-30 13:39:05 +0400

    Add MongoDB connection and database setup

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 app.use(express.json());
+
+let db;
+
+MongoClient.connect('mongodb+srv://mindhadi5_db_user:Password@cluster0.289bhfc.mongodb.net', (err, client) => {
+    if (err) {
+        console.error('Database connection error:', err);
+        return;
+    }
+    db = client.db('webstore');
+    console.log('Connected to MongoDB');
+});
 
 app.get('/', (req, res) => {
     res.send('Welcome to the webstore API');
