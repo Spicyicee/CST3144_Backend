9b83b91f825c6d9de1ef6d0aa63674b300484edd
Mohammad Hadi <mind.hadi5@gmai.com>
2025-12-01 11:55:20 +0400

    CORS middleware configured

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 
 app.use(express.json());
 
+// CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    next();
+});
+
 let db;
 
 MongoClient.connect('mongodb+srv://mindhadi5_db_user:Password@cluster0.289bhfc.mongodb.net', (err, client) => {
@@ -16,7 +25,7 @@ MongoClient.connect('mongodb+srv://mindhadi5_db_user:Password@cluster0.289bhfc.m
 });
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the webstore API');
+    res.send('Select a collection, e.g., /collection/messages');
 });
 
 const port = process.env.PORT || 3000;
