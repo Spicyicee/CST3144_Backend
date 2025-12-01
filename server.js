11d20d503e267fb06ff74830262de4c6ae531c8d
Mohammad Hadi <mind.hadi5@gmai.com>
2025-12-01 13:06:46 +0400

    Made changes to the search

--- server.js
+++ server.js
@@ -1,56 +1,56 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
 const app = express();
-
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID= require('mongodb').ObjectID;
+ 
 app.use(express.json());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+// app.set('port', 3000)
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Origin ,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+ 
     next();
+ 
 });
-
+ 
 let db;
-
-MongoClient.connect('mongodb+srv://mindhadi5_db_user:Password@cluster0.289bhfc.mongodb.net', (err, client) => {
-    if (err) {
-        console.error('Database connection error:', err);
-        return;
-    }
-    db = client.db('webstore');
-    console.log('Connected to MongoDB');
+ 
+MongoClient.connect('mongodb+srv://mindhadi5_db_user:Password@cluster0.289bhfc.mongodb.net', (err, client)=>{
+    db=client.db('webstore');
 });
-
-app.use((req, res, next) => {
-    console.log('Request IP:', req.url);
-    console.log('Request date:', new Date());
+ 
+app.use(function(req, res, next) {
+    console.log("Request IP: " + req.url);
+    console.log("Request date: " + new Date());
     next();
 });
 
-app.get('/', (req, res) => {
-    res.send('Select a collection, e.g., /collection/messages');
+app.get('/', (req, res, next)=>{
+    res.send('Select a collection, e.g., /collection/messages')
 });
-
-app.param('collectionName', (req, res, next, collectionName) => {
-    req.collection = db.collection(collectionName);
+ 
+app.param('collectionName', (req, res, next, collectionName)=>{
+    req.collection = db.collection(collectionName)
     return next();
 });
-
-app.get('/collection/:collectionName', (req, res, next) => {
-    req.collection.find({}).toArray((e, results) => {
-        if (e) return next(e);
-        res.send(results);
-    });
+ 
+//retrieve all the objects from a collection
+app.get('/collection/:collectionName', (req, res,next)=>{
+    req.collection.find({}).toArray((e, results)=>{
+        if (e) return next(e)
+ 
+            res.send(results)
+    })
 });
 
-app.post('/collection/:collectionName', (req, res, next) => {
-    req.collection.insert(req.body, (e, results) => {
-        if (e) return next(e);
-        res.send(results.ops);
-    });
+app.post('/collection/:collectionName', (req, res, next)=>{
+    req.collection.insert(req.body, (e, results)=>{
+        if (e) return next(e)
+        res.send(results.ops)
+
+    })
 });
 
 app.get('/collection/:collectionName/search/:query', (req, res, next) => {
@@ -91,37 +91,31 @@ app.get('/collection/:collectionName/search/:query', (req, res, next) => {
     });
 });
 
-app.get('/collection/:collectionName/:id', (req, res, next) => {
-    req.collection.findOne({_id: new ObjectID(req.params.id)}, (e, result) => {
-        if (e) return next(e);
-        res.send(result);
-    });
+
+app.get('/collection/:collectionName/:id', (req, res, next)=>{
+    req.collection.findOne({_id: new ObjectID(req.params.id)}, (e, result)=>{
+        if (e) return next(e)
+        res.send(result)
+    })
 });
 
-app.put('/collection/:collectionName/:id', (req, res, next) => {
-    req.collection.update(
-        {_id: new ObjectID(req.params.id)},
+app.put('/collection/:collectionName/:id', (req, res, next)=>{
+    req.collection.update({
+        _id: new ObjectID(req.params.id)},
         {$set: req.body},
-        {safe: true, multi: false},
-        (e, result) => {
-            if (e) return next(e);
-            res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'});
-        }
-    );
+        {safe: true, multi: false}, (e, result)=>{
+            if (e) return next(e)
+            res.send((result.result.n===1)?{msg: 'success'}:{msg: 'error'})
+        })
 });
 
-
-app.delete('/collection/:collectionName/:id', (req, res, next) => {
-    req.collection.deleteOne(
-        {_id: new ObjectID(req.params.id)},
-        (e, result) => {
-            if (e) return next(e);
-            res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'});
-        }
-    );
-});
+app.delete('/collectione/:collectionName/:id', (req, res, next) => { 
+    req.collection.deleteOne( {_id: ObjectID(req.params.id)},
+       (e, result) => { if (e) return next(e) 
+        res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'}) }) })
+ 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+app.listen(port,()=>{
+    console.log("Express.js server running at localhost:3000")
+})
